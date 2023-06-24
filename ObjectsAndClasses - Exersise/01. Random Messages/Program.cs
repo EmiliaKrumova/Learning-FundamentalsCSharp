1df5468cc5f0fb445950764d88d3202e4d767403
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-06-25 02:35:18 +0300

    Create new exercise - Objects and Classes with problems
    First exercise is with Random class

---
+++ ObjectsAndClasses - Exersise/01. Random Messages/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Random_Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phrases = new string[] { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can't live without this product." };
+            string[] events = new string[] { "Now I feel good.", "I have succeeded with this product.", "Makes miracles.I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+            string[] names = new string[] { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            string[] cities = new string[] { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+            int numberOfMessages = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            for (int i = 0; i < numberOfMessages; i++)
+            {
+                string phrase = phrases[random.Next(phrases.Length)];
+                string evens = events[random.Next(events.Length)];
+                string name = names[random.Next(names.Length)];
+                string city = cities[random.Next(cities.Length)];
+
+                Console.WriteLine($"{phrase} {evens} {name} – {city}.");
+            }
+
+        }
+    }
+}
