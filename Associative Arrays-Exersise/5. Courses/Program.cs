75459800a67da4c9dacb48e0b2d3b8fa434cc989
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-04 03:01:25 +0300

    added exercise Dictionaries

---
+++ Associative Arrays-Exersise/5. Courses/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _5._Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();// създавам речник с ключ - името на курса и стойност - списък с имената на студентите
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] courseInfo = command.Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+                string course = courseInfo[0];
+                string student = courseInfo[1];
+                if (!courses.ContainsKey(course))// ако този ключ не се съдържа в речника
+                {
+                    courses[course] = new List<string>();// създавам празен списък
+                }
+                courses[course].Add(student);// добавям студент в съответният курс
+            }
+            foreach (var kvp in courses)// взимам всички двойки в речника
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count}");// принтирам за всеки курс - колко броя студенти има в неговия списък
+                foreach(var student in kvp.Value)// вложен цикъл за всеки студент в списъка
+                {
+                    Console.WriteLine($"-- {student}");// изписвам името на студента
+                }
+            }
+        }
+    }
+}
---
+++ Associative Arrays-Exersise/6. Student Academy/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _6._Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int count = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+            for (int i = 0; i < count; i++)
+            {
+                string student = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+                if (!students.ContainsKey(student))
+                {
+                    students[student] = new List<double>();
+                }
+                students[student].Add(grade);
+
+            }
+            foreach(var kvp in students)
+            {
+                double avrGrade = kvp.Value.Average(x => x);
+                if (avrGrade < 4.50)
+                {
+                    students.Remove(kvp.Key);
+                }else
+                {
+                    Console.WriteLine($"{kvp.Key} -> {kvp.Value.Average(x => x):f2}");
+                }
+                
+            }
+        }
+    }
+}
