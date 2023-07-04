3739c3a5c2bd2f1fb6d46e630061585518e8f4f1
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-04 03:46:51 +0300

    Added Dictionaries-Lab

---
+++ Dictionaries, Lambda and LINQ-Lab/1. Count Real Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select (int.Parse)
+                .ToList();
+            SortedDictionary<int, int> counts = new SortedDictionary<int, int>();
+            foreach (int number in numbers)
+            {
+                if (!counts.ContainsKey(number))
+                {
+                    counts[number] = 0;
+                }
+                counts[number]++;
+            }
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key}-> {number.Value}");
+            }
+        }
+    }
+}
