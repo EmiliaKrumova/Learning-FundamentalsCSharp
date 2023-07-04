0dbed45292985e9ce4bcc23f61cf48a889be7035
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-04 03:15:19 +0300

    Dictionaries-Exercise

--- Associative Arrays-Exersise/7. Company Users/Program.cs
+++ Associative Arrays-Exersise/7. Company Users/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string,List<string>> companies = new Dictionary<string,List<string>>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "End") 
+            {
+                string[] cmdArg = cmd.Split(" -> ");
+                string compName = cmdArg[0];
+                string employeeID = cmdArg[1];
+                if (!companies.ContainsKey(compName))
+                {
+                    companies[compName] = new List<string>();
+                }
+                if (companies[compName].Contains(employeeID))
+                {
+                    continue;
+                }
+                companies[compName].Add(employeeID);
+            }
+            foreach (var kvp in  companies)
+            {
+                Console.WriteLine(kvp.Key);
+                foreach (var iD in kvp.Value)
+                {
+                    Console.WriteLine($"-- {iD}");
+                }
+
+            }
         }
     }
 }
