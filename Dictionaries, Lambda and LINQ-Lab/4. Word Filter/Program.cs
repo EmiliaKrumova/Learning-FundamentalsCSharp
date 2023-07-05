f6245f8de23c844d488cc556271c30d3ab163f7f
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-05 13:52:15 +0300

    Submit all tasks in Dictionary-Lab

---
+++ Dictionaries, Lambda and LINQ-Lab/4. Word Filter/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _4._Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                if (word.Length %2 == 0)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
