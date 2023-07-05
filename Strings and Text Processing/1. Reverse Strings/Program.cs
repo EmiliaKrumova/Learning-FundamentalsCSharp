d2c7c09734376d5f193cd904b2c2e68492bdcb7e
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-06 00:02:31 +0300

    added Text Processing Lab

---
+++ Strings and Text Processing/1. Reverse Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _1._Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            while ((word = Console.ReadLine()) != "end")
+            {
+                string reversed = string.Empty;
+                for (int i = word.Length-1; i >= 0; i--)
+                {
+                    reversed += word[i];
+                }
+                Console.WriteLine($"{word} = {reversed}");
+            }
+            
+        }
+    }
+}
---
+++ Strings and Text Processing/5. Digits, Letters and Other/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _5._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string digits = string.Empty;
+            string letters = string.Empty;
+            string otherChars = string.Empty;
+            for (int i = 0;i< text.Length;i++)
+            {
+                char curChar = text[i];
+               if (char.IsDigit(curChar))
+                {
+                    digits += curChar;
+
+                }else if (char.IsLetter(curChar))
+                {
+                    letters += curChar;
+                }
+                else
+                {
+                    otherChars += curChar;
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(otherChars);
+        }
+    }
+}
