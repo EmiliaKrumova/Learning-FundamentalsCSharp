ccf713915560bbd36eb488ee543a035fb7e6e858
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-05 11:23:11 +0300

    Dictionaries-Lab - added Task3

---
+++ Dictionaries, Lambda and LINQ-Lab/3. Word Synonyms/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _3._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> synonims = new Dictionary<string, List<string>>();
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                string keyWord = Console.ReadLine();
+                if (!synonims.ContainsKey(keyWord))
+                {
+                    synonims[keyWord] = new List<string>();
+                }
+                string synonimWord = Console.ReadLine();
+                synonims[keyWord].Add(synonimWord);
+            }
+
+            foreach( KeyValuePair<string, List<string>> keyValuePair in synonims )
+            {
+                Console.WriteLine($"{keyValuePair.Key} - {string.Join(", ", keyValuePair.Value)}");
+            }
+            
+        }
+    }
+}
