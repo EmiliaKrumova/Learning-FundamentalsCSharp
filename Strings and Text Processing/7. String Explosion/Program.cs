625581aa3b8b8a06a121254ee24916f8371b7a61
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-08 01:51:06 +0300

    added task6 and 7

---
+++ Strings and Text Processing/7. String Explosion/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace _7._String_Explosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder output = new StringBuilder();
+            int bombpower = 0;
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currChar = input[i];
+                
+                if (currChar == '>')
+                {
+                    int currBombPower = GetValueOfChar(input[i + 1]);
+                    output.Append(currChar);
+                    bombpower += currBombPower;
+
+
+                }
+                else
+                {
+                    if (bombpower > 0)
+                    {
+                        bombpower--;
+                    }
+                    else
+                    {
+                        output.Append(currChar);
+                    }
+                }
+                
+               
+            }
+            Console.WriteLine(output.ToString());
+        } static int GetValueOfChar (char c)
+        {
+            
+            return (int)c - 48;
+        }
+    }
+}
