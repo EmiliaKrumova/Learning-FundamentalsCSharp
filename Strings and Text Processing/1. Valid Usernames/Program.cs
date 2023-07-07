e9cfaf4d080e68a34bd4396aa2ce748d10aa6aed
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-07 16:20:52 +0300

    added Task2 Multiply strings

---
+++ Strings and Text Processing/1. Valid Usernames/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _1._Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine().Split(", ").ToList();
+
+            
+            foreach (string inputItem in input)
+            {
+                bool isValid = true;
+                if (inputItem.Length<3 || inputItem.Length > 16)
+                {
+                    isValid = false;
+                    continue; ;
+                }
+                for (int i = 0; i < inputItem.Length; i++)
+                {
+                    char currChar = inputItem[i];
+                    
+                    if (!((char.IsLetterOrDigit(currChar) || currChar == '-' || currChar == '_')))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                
+                
+                }
+                if (isValid)
+                {
+                    Console.WriteLine(inputItem);
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ Strings and Text Processing/2. Character Multiplier/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _2._Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+            string firstWord = words[0];
+            string secondWord = words[1];
+            int lentgth = Math.Min(firstWord.Length, secondWord.Length);
+            int sum = 0;
+            for (int i = 0; i < lentgth; i++)
+            {
+                char currFirstchar = (char)firstWord[i];
+                char currSecondChar = (char)secondWord[i];
+                sum += currFirstchar * currSecondChar;
+                
+            }
+            if (firstWord.Length > lentgth)
+            {
+                for (int i = lentgth; i < firstWord.Length; i++)
+                {
+                    sum += firstWord[i];
+                }
+            }
+            else if (secondWord.Length > lentgth)
+            {
+                for (int i = lentgth;i < secondWord.Length; i++)
+                {
+                    sum += secondWord[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
