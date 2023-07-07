ebfe78fd3c4e487e86f3e3df46b32bea58b02a44
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-07 18:02:48 +0300

    added Text Processing exercises

---
+++ Strings and Text Processing/3. Extract File/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _3._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string input = Console.ReadLine();
+            int indexOfSlash = input.LastIndexOf(@"\");
+            int indexOfDot = input.LastIndexOf(".");
+            string fileName = input.Substring(indexOfSlash+1, (indexOfDot-1)-indexOfSlash);
+            string extention = input.Substring(indexOfDot+1,input.Length-(indexOfDot+1));
+
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extention}");
+        }
+    }
+}
---
+++ Strings and Text Processing/4. Caesar Cipher/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _4._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string encriptedText = string.Empty;
+            for(int i = 0; i< input.Length; i++)
+            {
+                char curChar = input[i];
+                encriptedText += (char)(curChar+3);
+            }
+            Console.WriteLine(encriptedText);
+        }
+    }
+}
