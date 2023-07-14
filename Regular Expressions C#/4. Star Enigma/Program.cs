480fe5f8be1f37c93e4d060945807a8140e7f4b4
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-15 01:16:20 +0300

    Added task5 with Methods

---
+++ Regular Expressions C#/4. Star Enigma/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _4._Star_Enigma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<string> attacked = new List<string>();
+            List<string>destroyed = new List<string>();
+            for(int i = 0; i < lines; i++)
+            {
+                string encriptedMsg = Console.ReadLine();
+                string decrypted = DecriptionMethod(encriptedMsg);
+                string patern = "[^\\@\\!\\-\\:\\>]*\\@(?<planet>[A-Za-z]+)[^\\@\\!\\-\\:\\>]*\\:\\d+[^\\@\\!\\-\\:\\>]*\\!(?<atackType>A|D)\\![^\\@\\!\\-\\:\\>]*\\-\\>\\d+";
+                Regex regex = new Regex(patern);
+                Match match = regex.Match(decrypted);
+                if (match.Success)
+                {
+                    string planetName = match.Groups["planet"].Value;
+                    string atackType = match.Groups["atackType"].Value;
+                    if (atackType == "A")
+                    {
+                        attacked.Add(planetName);
+                    }
+                    else if (atackType == "D")
+                    {
+                        destroyed.Add(planetName);
+                    }
+                }
+               
+            }PrintPlanets(attacked, "Attacked");
+            PrintPlanets(destroyed, "Destroyed");
+            
+        }
+        static void PrintPlanets (List<string> planets,string atackType)
+        {
+            Console.WriteLine($"{atackType} planets: {planets.Count}");
+            foreach (string planet in planets.OrderBy(x => x))
+             {
+
+                Console.WriteLine($"-> {planet}");
+            }
+        }
+        static string DecriptionMethod (string encriptedMsg)
+        {
+            StringBuilder decryptedMsg = new StringBuilder();
+            int count = CountOfSTAR(encriptedMsg);
+            foreach (char curChar in encriptedMsg)
+            {
+                decryptedMsg.Append((char)(curChar-count));
+            }
+            return decryptedMsg.ToString();
+        }
+        static int CountOfSTAR(string encriptedMsg)
+        {
+            int count = 0;
+            foreach(char s in encriptedMsg.ToLower())
+            {
+                if (s == 's'|| s=='t'|| s=='a'|| s == 'r')
+                {
+                    count++;
+                }
+                
+            }
+            return count;
+        }
+    }
+}
---
+++ Regular Expressions C#/5. Nether Realms/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _5._Nether_Realms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] demons = Regex.Split(Console.ReadLine(), @" *,{1} *");// сплитвам по регекс ,който може да е интервал (0 или повече), запетайка и пак интервал, за да направя масив с имената на демоните
+
+            SortedDictionary<string,Dictionary<int, double>> demonsDict = new SortedDictionary<string,Dictionary<int, double>>();// Речник за информацията за демоните
+           
+
+            for (int i = 0; i < demons.Length; i++)
+            {
+                int currDemonHealt = 0;// живот на демона
+                string currDemonName = demons[i];// име на демона
+               
+                string healthPattern = "[^\\*\\-\\+\\/\\.\\d]"; // създавам патерн, който игнорира математическите знаци, точка и цифри
+                Regex health = new Regex(healthPattern, RegexOptions.None);
+                
+                MatchCollection matchHealth = health.Matches(currDemonName);// правя колекция от всички символи , без тези в патерна
+                foreach (Match matchChar in matchHealth)
+                {
+                   char currChar =char.Parse( matchChar.Value);
+                    currDemonHealt += currChar;
+                }
+
+
+                double currDamage = 0;
+                string damagePattern = "((|-)\\d+\\.\\d+|(|-)\\d+)";
+                Regex damage = new Regex(damagePattern);
+                MatchCollection matchDmg = damage.Matches(currDemonName);
+                foreach (Match matchNumber in matchDmg)
+                {
+                    double currDmg = double.Parse( matchNumber.Value);
+                    currDamage += currDmg;
+                }
+                foreach(char ch in currDemonName)
+                {
+                    if (ch ==  '*')
+                    {
+                        currDamage *= 2;
+                    }else if (ch == '/')
+                    {
+                        currDamage/=2;
+                    }
+                }
+                if (!demonsDict.ContainsKey(currDemonName))
+                {
+                    demonsDict[currDemonName] = new Dictionary<int, double>();
+                    if (!demonsDict[currDemonName].ContainsKey(currDemonHealt))
+                    {
+                        demonsDict[currDemonName][currDemonHealt] = currDamage;
+                    }
+                }
+
+            }
+            foreach(var demon in demonsDict)
+            {
+
+                Dictionary<int, double> secondDict = demon.Value;
+               // Console.WriteLine($"{demon.Key} - {demon.Value.Keys.SingleOrDefault()} health, {demon.Value.Values.Single():f2} damage");
+
+                foreach(var key in secondDict)
+                {
+                    Console.WriteLine($"{demon.Key} - {key.Key} health, {key.Value:f2} damage");
+                }
+
+                
+            }
+            
+        }
+    }
+}
