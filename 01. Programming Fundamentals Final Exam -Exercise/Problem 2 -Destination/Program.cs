ad23aa063f0ebdc7c214452fe8c075e0443f1c81
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-17 03:58:34 +0300

    added Problem2-DestonationsMapping

---
+++ 01. Programming Fundamentals Final Exam -Exercise/Problem 2 -Destination/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Problem_2__Destination
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = "(=|/)(?<location>[A-Z][A-Za-z]{2,})\\1";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+            int points = 0;
+            List<string> destinations = new List<string>();
+            foreach (Match match in matches)
+            {
+                points += match.Groups["location"].Length;
+                destinations.Add(match.Groups["location"].Value);
+            }
+           // Console.Write("Destinations: ");
+           // Console.WriteLine(String.Join(", ", destinations));
+            Console.WriteLine($"Travel Points: {points}");
+            Console.WriteLine($"Destinations: {(String.Join(", ", destinations))}");
+        }
+    }
+}
