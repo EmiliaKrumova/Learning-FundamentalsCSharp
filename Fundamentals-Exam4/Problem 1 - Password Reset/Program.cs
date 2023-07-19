9a16e6435f3797a5757ed025ebefea9b24aadf3c
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-20 01:28:48 +0300

    Add Fancy BarcodeRegex Problem

---
+++ Fundamentals-Exam4/Problem 1 - Password Reset/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+
+namespace Problem_1___Password_Reset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string command;
+           
+            StringBuilder sb = new StringBuilder();
+            string password = input;
+
+            while ((command = Console.ReadLine()) != "Done")
+            {
+                string[] commandArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string realCom = commandArgs[0];
+
+                if (realCom == "TakeOdd")
+                {
+                    password = TakeODD(sb, password);
+
+                }
+                else if (realCom == "Cut")
+                {
+                    password = Cut(password, commandArgs);
+
+                }
+                else if (realCom == "Substitute")
+                {
+                    password = Substitute(password, commandArgs);
+                }
+            }     
+                Console.WriteLine($"Your password is: {password}");
+            
+            
+        }
+
+        private static string Substitute(string password, string[] commandArgs)
+        {
+            string substring = commandArgs[1];
+            string substitute = commandArgs[2];
+            if (password.Contains(substring))
+            {
+                password = password.Replace(substring, substitute);
+                Console.WriteLine(password);
+            }
+            else
+            {
+                Console.WriteLine("Nothing to replace!");
+            }
+
+            return password;
+        }
+
+        private static string Cut(string password, string[] commandArgs)
+        {
+            int index = int.Parse(commandArgs[1]);
+            int lenght = int.Parse(commandArgs[2]);
+            password = password.Remove(index, lenght);
+            Console.WriteLine(password);
+            return password;
+        }
+
+        private static string TakeODD(StringBuilder sb, string password)
+        {
+            for (int i = 1; i < password.Length; i += 2)
+            {
+                sb.Append(password[i]);
+            }
+            password = sb.ToString();
+            Console.WriteLine(password);
+            return password;
+        }
+    }
+}
---
+++ Fundamentals-Exam4/Problem 2-Fancy Barcodes/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Problem_2_Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string pattern = "(\\@#+)(([A-Z][A-Za-z0-9]{4,})[A-Z])\\@#+";
+            int n = int.Parse(Console.ReadLine());
+            //Dictionary<Match,int> map = new Dictionary<Match,int>();
+            for(int i = 0; i < n; i++)
+            {
+                //string pattern = "(\\@#+)(([A-Z][A-Za-z0-9]{5,})[A-Z])\\1";
+                string input = Console.ReadLine();
+                Regex regex1 = new Regex(pattern);
+                Match match  = regex1.Match(input);
+                if(!match.Success)
+                {
+                    Console.WriteLine("Invalid barcode");
+                    continue;
+
+                }
+                string secondPatern = "\\d";
+                Regex regex = new Regex(secondPatern);
+                string productGroup = string.Empty;
+
+               
+
+                MatchCollection digits = regex.Matches(match.ToString());
+                
+                if (digits.Count > 0)
+                {
+                    
+                    foreach (Match m in digits)
+                    {
+                        productGroup += m.Value;
+
+                    }
+                }
+                else
+                {
+                    productGroup += "00";
+                    
+                }
+                
+               
+                Console.WriteLine($"Product group: {productGroup}");
+            }
+           
+        }
+    }
+}
