986bb47ca5f4f1f88196b5a7ce956d2fb44e89a3
EmiliaKrumova <132586170+EmiliaKrumova@users.noreply.github.com>
2023-07-27 02:13:39 +0300

    Added Car Race.cs

---
+++ Lists - More Exercise/01. Messaging/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace _01._Messaging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //9992 562 8933
+           // This is some message for you
+           // 0123456789112345678921234567        
+                        string [] separateDigits = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string text = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < separateDigits.Length; i++)
+            {
+                int currIndex = 0;
+                int  currNum = int.Parse(separateDigits[i]);
+
+                while(currNum > 0)
+                {
+                    currIndex += currNum % 10;
+                    currNum = currNum / 10;
+
+                }
+                if(currIndex>text.Length)
+                {
+                    currIndex = currIndex%text.Length;
+                }
+                sb.Append(text[currIndex]);
+                text = text.Remove(currIndex,1);
+              
+                
+            }
+            Console.WriteLine(sb.ToString());
+        } 
+    }
+}
---
+++ Lists - More Exercise/2. Car Race/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _2._Car_Race
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            //The time of the left racer is (29 + 13 + 9) * 0.8 (because of the zero) + 13 = 53.8
+            //The time of the right racer is (82 + 12 + 14) * 0.8 + 21 = 107.4
+
+            List<int> list = Console.ReadLine()
+                .Split(" ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            double leftTime = 0;
+            double rightTime = 0;
+            for(int i = 0; i < list.Count/2; i++)
+            {
+                if (list[i] == 0)
+                {
+                    leftTime *= 0.8;
+                }
+                else
+                {
+                    leftTime += list[i];
+                }
+                
+            }
+            //Console.WriteLine(leftTime);
+            //29 13 9 0 13 0 21 0 14 82 12 
+            for (int i = list.Count-1;i>list.Count/2; i--)
+            
+            {
+                if (list[i] == 0)
+                {
+                    rightTime *= 0.8;
+                }
+                else
+                {
+                    rightTime += list[i];
+                }
+
+            }//Console.WriteLine(rightTime);
+            if (leftTime > rightTime)
+            {
+                Console.WriteLine($"The winner is right with total time: {rightTime:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is left with total time: {leftTime:f2}");
+            }
+        }
+    }
+}
